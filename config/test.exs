588888a8ae83a626d504cd0cc13858f38a2af0af
Thomas Punt <tpunt@hotmail.co.uk>
2017-08-04 15:19:25 +0100

    Reduce pool size to make deadlocks in the DB less likely

--- config/test.exs
+++ config/test.exs
@@ -19,6 +19,6 @@ config :neo4j_sips, Neo4j,
     username: System.get_env("NEO4J_USERNAME_TEST"),
     password: System.get_env("NEO4J_PASSWORD_TEST")
   ],
-  pool_size: 20,
+  pool_size: 10,
   max_overflow: 2,
   timeout: 5_000
