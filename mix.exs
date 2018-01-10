522f23ba36fdf675b044faa80e1f1209955fd3c1
Thomas Punt <tpunt@hotmail.co.uk>
2018-01-10 18:18:47 +0000

    Make a new release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhpInternals.Mixfile do
 
   def project do
     [app: :php_internals,
-     version: "0.0.9",
+     version: "0.0.10",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
